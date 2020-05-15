e66f2e938ec40c5bbe80c908d0f9e28e1724b3fe
Laura Goon <goon.laura@gmail.com>
2020-05-15 11:54:24 -0400

    burger menu pt 4

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
+// general
 $(document).ready(function(){
  $('.header').height($(window).height());
  
 })
 
+// carousel
+
 $('.carousel').carousel({
   interval: 8000
+}
+
+// nav menu
+var burgerMenu = document.querySelector(#nav-burger);
+burgerMenu.addEventListener("click", showMenu, false);
+
+var menuList = document.querySelector(#nav-list);
+menuList.addEventListener("click", hideMenu, false);
+
+function showMenu(e) {
+	menuList.classList.add("show");
+	document.body.style.overflow = "hidden";
+}
+
+function hideMenu(e) {
+	menuList.classList.remove("show");
+	e.stopPropagation();
+	document.body.style.overflow = "auto";
 }

6d9c0e34c50df93191c23f9a60dbd48377887a1b
Laura Goon <goon.laura@gmail.com>
2020-05-17 10:50:10 -0400

    animate index pt 6

--- main.js
+++ main.js
@@ -5,21 +5,64 @@ $(document).ready(function(){
 })
 
 // typing header
-var headerText = $('.typer').text();
-console.log(headerText);
-var headerLength = headerText.length;
-console.log(headerLength);
+var headerOneText = $('#header-one').text();
+// console.log(headerText);
+var headerOneLength = headerOneText.length;
+// console.log(headerLength);
+var headerTwoText = $('#header-two').text();
+var headerTwoLength = headerTwoText.length;
+var headerThreeText = $('#header-three').text();
+var headerThreeLength = headerThreeText.length;
+var headerFourText = $('#header-four').text();
+var headerFourLength = headerFourText.length;
+var totalLength = headerOneLength + headerTwoLength + headerThreeLength + headerFourLength;
 var timeOut;
 var char = 0;
+var curr = 0;
 
 (function typeWriter() {
 	timeOut = setTimeout(function() {
+		curr++;
 		char++;
-		var type = headerText.substring(0, char);
-		$('.typer').text(type);
-		typeWriter();
 
-		if (char == headerLength) {
+		if (char < headerOneLength) {
+			var type = headerOneText.substring(0, curr);
+			$('#header-one').text(type);
+			typeWriter();
+
+			if (char == headerOneLength) {
+				curr = 0;
+			}
+		}
+
+		else if (char < headerOneLength + headerTwoLength) {
+			var type = headerTwoText.substring(0, curr);
+			$('#header-two').text(type);
+			typeWriter();
+
+			if (char == headerOneLength + headerTwoLength) {
+				curr = 0;
+			}
+		}
+
+		else if (char < headerOneLength + headerTwoLength + headerThreeLength) {
+			var type = headerThreeText.substring(0, curr);
+			$('#header-three').text(type);
+			typeWriter();
+
+			if (char == headerOneLength + headerTwoLength + headerThreeText) {
+				curr = 0;
+			}
+		}
+
+		else {
+			var type = headerFourText.substring(0, curr);
+			$('#header-four').text(type);
+			typeWriter();
+		}
+
+
+		if (char == totalLength) {
 			clearTimeout(timeOut);
 		}
 

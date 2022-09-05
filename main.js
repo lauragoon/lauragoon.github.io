17ba3eccf662d19818fa7758af77d9065a26c686
Laura Goon <goon.laura@gmail.com>
2022-09-05 00:17:43 -0700

    logo and lamp matches header color and hover color

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ function switchMode()
     if (window.getComputedStyle(document.body).backgroundColor === "rgb(34, 29, 35)")
     {
         document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById('logo-img').src = "media/logo-for-light.png";
+        document.getElementById('theme-switch-img-default').src = "media/lamp-for-light.png";
+        document.getElementById('theme-switch-img-hover').src = "media/lamp-for-light-hover.png";
     }
     else
     {
         document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById('logo-img').src = "media/logo-for-dark.png";
+        document.getElementById('theme-switch-img-default').src = "media/lamp-for-dark.png";
+        document.getElementById('theme-switch-img-hover').src = "media/lamp-for-dark-hover.png";
     }
 }
